c3123f76229432d76308eb996eb2404db40079c8
zhangxinxin <zhangxinxin12@100tal.com>
2024-02-02 17:23:55 +0800

    first commit

---
+++ 递归/helloworld/helloworld.js
@@ -0,0 +1,5 @@
+
+function function_name(argument) {
+	console.log('Hello World JS');
+}
+function_name()
---
+++ 递归/链表 --- 反转链表/helloworld.py
@@ -0,0 +1,8 @@
+
+def function():
+    pass
+    print('hello world Python')
+
+
+if __name__ == '__main__':
+    function()
